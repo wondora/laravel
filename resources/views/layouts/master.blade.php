087ecedb3ba3c0dc4f70fffb84dd2d9870166a03
wondora <onemimo@outlook.com>
2019-02-15 16:05:53 +0900

    새로운 라라벨 프로젝터 생성

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>라라벨 입문</title>
+</head>
+<body>
+    @yield('content')
+    @yield('script')
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.master')
+
+@section('content')
+    @include('partials.footer')
+@endsection
+
+@section('script')
+ <script>
+    alert("저는 자식 뷰의 script 섹션입니다.");
+ </script>
+@endsection
